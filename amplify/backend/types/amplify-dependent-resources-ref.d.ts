5571f0ed5f9568f8fcc90db85e83996297ab1b21
khizar <khizaras@gmail.com>
2022-08-12 23:48:31 +0530

    initiated amplify and added functions

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "aggregatorsOperation": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
